567687b48a983bb628bbe06c6f1339f6f830b279
1dd0Rv$ <vidalsanchezroddy7@gmail.com>
2026-05-08 10:47:51 +0200

    Delete scanner_pkg directory

--- scanner_pkg/__init__.py
+++
@@ -1,4 +0,0 @@
-from .scanner import escanear_host
-from .servicios import obtener_servicio, obtener_banner
-from .os_detect import detectar_os
-from .red import parsear_puertos, parsear_hosts, resolver_hostname
